904014ebefa67e17735c0c157fd7aaea8b132980
Whopperli <leonli1315@gmail.com>
2022-06-12 00:25:36 +0800

    First

---
+++ src/main/java/demo/controller/HelloWorldController.java
@@ -0,0 +1,30 @@
+package demo.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+ 
+@Controller
+public class HelloWorldController {
+ 
+	@RequestMapping(value = "/redirect", method = RequestMethod.GET)
+    public String authorInfo(Model model) {        
+        return "redirect:/hello";
+    }
+
+    @RequestMapping("/hello")
+    public String hello(Model model) {         
+        model.addAttribute("greeting", "Hello Spring MVC");         
+        return"helloworld";         
+    }
+ 
+    @RequestMapping(value = "/text" , produces="application/html;charset=utf8")
+    @ResponseBody
+    public String authorInfo2(Model model) {
+    	//model.addAttribute("info","ResponseBody Info");
+        return "這是純文字";
+    } 
+
+}
+
